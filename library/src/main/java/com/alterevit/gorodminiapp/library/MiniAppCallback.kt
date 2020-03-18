7cd324429d2c30ac13c6c6d8d859382c79436b4a
krosd <krosdmitrij@yandex.ru>
2020-03-18 16:15:10 +0300

    Callback: добавлен метод pushFragment(Fragment)

--- library/src/main/java/com/alterevit/gorodminiapp/library/MiniAppCallback.kt
+++ library/src/main/java/com/alterevit/gorodminiapp/library/MiniAppCallback.kt
@@ -1,7 +1,11 @@
 package com.alterevit.gorodminiapp.library
 
+import androidx.fragment.app.Fragment
+
 interface MiniAppCallback{
 
     fun getUid(): String?
+
+    fun pushFragment(fragment: Fragment)
 
 }
